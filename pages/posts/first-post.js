a6a87863637fa4e32828ed61b9a4d6030b7c5563
mufti <murobbani18@gmail.com>
2022-04-22 11:49:20 +0700

    initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Script from 'next/script'
+
+export default function FirstPost(){
+    return (
+        <>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() => 
+                    console.log(`script loaded correctly, window.F8 has been populated`)
+                }
+            />
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </>
+    )
+}
